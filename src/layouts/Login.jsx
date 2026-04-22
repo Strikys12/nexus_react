c1155ff431dab4dbfcda11b02bb92bd313dc325d
Strikys12 <daqui.99@hotmail.com>
2026-04-21 21:00:16 -0500

    Se incorpora  estilos en los logins

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -3,60 +3,69 @@ import { Link, useNavigate } from "react-router-dom";
 import { apiFetch, endpoints } from "../services/api";
 import { successAlert, errorAlert } from "../helpers/alerts";
 
+import "../layouts/login.css";// Asegúrate de que el archivo se llame exactamente así
+
 export default function Login() {
-
   const navigate = useNavigate();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
-
       await apiFetch(endpoints.login, {
         method: "POST",
         body: JSON.stringify({ email, password }),
       });
-
       localStorage.setItem("token", "fake-token");
-
       successAlert("Login exitoso");
-
       navigate("/services");
-
     } catch (error) {
       errorAlert("Credenciales inválidas");
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="fondo"> {/* Mapea con .fondo del CSS */}
+      <div className="contenedor-login"> {/* Mapea con .contenedor-login */}
+        
+        <div className="encabezado"> {/* Mapea con .encabezado */}
+          <div className="logo">NEXUS</div>
+          <h2>Bienvenido</h2>
+          <p>Ingresa tus credenciales para continuar</p>
+        </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Correo"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <form onSubmit={handleSubmit}>
+          <label>Correo Electrónico</label>
+          <input
+            type="email"
+            placeholder="ejemplo@correo.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <label>Contraseña</label>
+          <input
+            type="password"
+            placeholder="••••••••"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-        <button type="submit">Ingresar</button>
-      </form>
+          <div className="botones"> {/* Mapea con .botones */}
+            <button type="submit">Ingresar</button>
+          </div>
+        </form>
 
-      <p>
-        ¿No tienes cuenta?
-        <Link to="/register"> Regístrate aquí</Link>
-      </p>
+        <footer>
+          <p>
+            ¿No tienes cuenta?
+            <Link to="/register" style={{ color: '#0b1f3a', fontWeight: 'bold' }}> Regístrate aquí</Link>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
